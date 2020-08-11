c3b06d7cad986a83b7ab940c7828da4e312d7740
David Gehtman <davidgehtman.91@gmail.com>
2020-08-11 10:49:08 -0400

    created a repo, added the most recent files

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import TextItem from './components/Textitem';
+import Header from './components/header';
+import './App.css';
+
+// const DATA_SIZE_HALF = "half"
+const DATA_SIZE_FULL = "full"
+const DATA_SIZE_HALF = "half"
+
+const INTERVAL_TIME = 2000
+
+/** Application entry point */
+function App() {
+
+  const [data, setData] = useState([])
+  const [value, setValue] = useState(0)
+  const [searchInput, setSearchInput] = useState("")
+
+
+  /** DO NOT CHANGE THE FUNCTION BELOW */
+  useEffect(() => {
+    setInterval(() => {
+      // Find random bucket of words to highlight
+      setValue(Math.floor(Math.random() * 10))
+    }, INTERVAL_TIME)
+  }, [])
+  /** DO NOT CHANGE THE FUNCTION ABOVE */
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch("/api/dataIdList?datasize=" + DATA_SIZE_HALF)
+      let list = await response.json()
+
+      let dataItems = await Promise.all (list.map(async id => {
+        return (await fetch("/api/dataItem/" + id)).json()
+      }))
+     
+      setData(dataItems)
+    }
+    
+    fetchData()
+  }, [])
+
+  const handleChange = e => {
+    setSearchInput(e.target.value)
+  }
+
+  return (
+    <div className="App">
+     <Header/>
+      <div className="SearchBar">
+        <input type="text" placeholder="Search text" value={searchInput} onChange={handleChange}/>
+      </div>
+     {
+       data.map((row, i) => {
+        return (<p key={`p${i}`}>
+          {row.map((textitem, j) => {
+            if (searchInput.length > 0 && textitem.text.search(searchInput) === -1) {
+              return null;
+            }
+
+            return (
+            <>
+              <TextItem key={`${i}${j}`} value={value} data={textitem}/>
+            </>)
+          })}
+        </p>)
+       })
+     }
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from'react';
+import './style.css';
+
+function Header(){
+    return(
+        <header>
+            <h1 className="title">
+                JusticeText
+            </h1>
+
+        </header>
+
+    )
+}
+
+export default Header;
